bb3bfb46a0f43f2b9b5a7afafdf74d23cc558f44
Nazir <anwernazir674@gmail.com>
2024-01-13 14:57:11 +0530

    init

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -9,6 +9,26 @@ router.post('/products', async (req, res) => {
   // 2. Create a new product using Product.create()
   // 3. Handle success: Respond with a 201 status code and the created product
   // 4. Handle errors: Respond with appropriate error messages and status codes
+
+  try{
+    const {productName,price}=req.body;
+
+   console.log("request body", productName,price);
+   const newProduct= await Product.create({
+        productName,
+        price
+   });
+   if(newProduct){
+    res.status(201).json({message:"Product created",data:newProduct});
+   }else{
+     res.status(500).json({message:"Internal server error"});
+   }
+   
+  }
+  catch(error){
+    console.log("error reason",error);
+    res.status(500).json({message:"Internal server error"});
+  }
 });
 
 // Retrieve a product by ID
@@ -18,6 +38,23 @@ router.get('/products/:id', async (req, res) => {
   // 2. Find the product by ID using Product.findById()
   // 3. Handle success: Respond with a 200 status code and the product data
   // 4. Handle errors: Respond with appropriate error messages and status codes
+  try{
+    const {id}=req.params;
+
+   console.log("request body",id);
+
+   const newProduct= await Product.findById(id);
+
+   if(newProduct){
+    res.status(200).json({message:"Product data",data:newProduct});
+   }else{
+     res.status(404).json({message:"Product not found"});
+   }
+   
+  }
+  catch(error){
+    res.status(500).json({message:"Internal server error"});
+  }
 });
 
 // Update a product by ID
@@ -28,6 +65,24 @@ router.patch('/products/:id', async (req, res) => {
   // 3. Use Product.findByIdAndUpdate() to update the product
   // 4. Handle success: Respond with a 200 status code and the updated product data
   // 5. Handle errors: Respond with appropriate error messages and status codes
+  try{
+    const {id}=req.params;
+    const {updatedContent}=req.body;
+
+   console.log("request body",id);
+
+   const newProduct= await Product.findByIdAndUpdate(id,updatedContent,{new : true});
+
+   if(newProduct){
+    res.status(200).json({message:"Product updated",data:newProduct});
+   }else{
+     res.status(404).json({message:"Product not found"});
+   }
+   
+  }
+  catch(error){
+    res.status(500).json({message:"Internal server error"});
+  }
 });
 
 // Delete a product by ID
@@ -37,6 +92,24 @@ router.delete('/products/:id', async (req, res) => {
   // 2. Use Product.findByIdAndDelete() to delete the product
   // 3. Handle success: Respond with a 200 status code and a deletion confirmation message
   // 4. Handle errors: Respond with appropriate error messages and status codes
+  try{
+    const {id}=req.params;
+    const {updatedContent}=req.body;
+
+   console.log("request body",id);
+
+   const newProduct= await Product.findByIdAndDelete(id);
+
+   if(newProduct){
+    res.status(200).json({message:"Product deleted",data:newProduct});
+   }else{
+     res.status(404).json({message:"Product not found"});
+   }
+   
+  }
+  catch(error){
+    res.status(500).json({message:"Internal server error"});
+  }
 });
 
 module.exports = router;
--- model/productModels.js
+++ model/productModels.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   // Implement the product schema fields here as
   // productName as String and price as Number
+   productName: {
+    type:String,
+    required:true},
+   price: {type:Number, required:true},
 });
 
 // Create and export the Product model
+module.exports=mongoose.model("Products",productSchema);
